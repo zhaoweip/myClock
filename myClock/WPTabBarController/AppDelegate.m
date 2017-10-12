f30f3ca8deaa3c3e60a64ea8e7ac9b1f7d7207eb
Macintosh <macintosh@Macintoshs-MacBook-Pro.local>
2017-10-12 10:32:06 +0800

    在delegate输入输出语句测试Xcode的GitHub功能

--- myClock/WPTabBarController/AppDelegate.m
+++ myClock/WPTabBarController/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "WPTabBarController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    WPTabBarController *tab = [[WPTabBarController alloc] init];
+    self.window.rootViewController = tab;
+    NSLog(@"test github");
+    
     return YES;
 }
 
