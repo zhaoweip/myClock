ea830d9d28f9cddfb4a8ec120a97fecb902acfa9
Macintosh <308324108@qq.com>
2017-10-17 10:30:28 +0800

    查询页面的图标上传以及页面的基本布局

--- myClock/Search/SearchPageViewController.m
+++ myClock/Search/SearchPageViewController.m
@@ -23,6 +23,7 @@
     // Do any additional setup after loading the view.
     [self setBackImage];
     [self setSearceContent];
+//    [self setDatePick];
     //设置导航控制器的代理为self，在代理方法里面去隐藏导航栏
     self.navigationController.delegate = self;
 
@@ -38,7 +39,6 @@
     //labelTagImg
     UIImageView *labelTagImg = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"label_tag.png"]];
     [self.view addSubview:labelTagImg];
-//    labelTagImg.backgroundColor = [UIColor redColor];
     [labelTagImg mas_makeConstraints:^(MASConstraintMaker *make) {
         make.width.mas_equalTo(12);
         make.height.mas_equalTo(12);
@@ -104,9 +104,91 @@
         make.left.equalTo(oldDataBtn.mas_left).offset(150);
     }];
     [newDataBtn addTarget:self action:@selector(dataBtnClick:) forControlEvents:UIControlEventTouchUpInside];
-
     
+    //日期输入框
+    UITextField *date = [[UITextField alloc] init];
+    date.backgroundColor = [UIColor whiteColor];
+    date.enabled = NO;
+    date.layer.cornerRadius = 10;
+    [self.view addSubview:date];
+    [date mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(oldDataBtn.mas_bottom).offset(20);
+        make.left.mas_equalTo(38);
+        make.right.mas_equalTo(-38);
+        make.height.mas_equalTo(45);
+    }];
+    
+    UIImageView *dateTag = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"date_tag.png"]];
+    [date addSubview:dateTag];
+    [dateTag mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.width.mas_equalTo(30);
+        make.height.mas_equalTo(30);
+        make.right.mas_equalTo(-10);
+        make.centerY.equalTo(date);
+        
+    }];
+    
+    UITextField *time = [[UITextField alloc] init];
+    time.backgroundColor = [UIColor whiteColor];
+    time.enabled = NO;
+    time.layer.cornerRadius = 10;
+    [self.view addSubview:time];
+    [time mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(date.mas_bottom).offset(10);
+        make.left.mas_equalTo(38);
+        make.right.mas_equalTo(-38);
+        make.height.mas_equalTo(45);
+    }];
+    
+    UIImageView *timeTag = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"date_tag.png"]];
+    [time addSubview:timeTag];
+    [timeTag mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.width.mas_equalTo(30);
+        make.height.mas_equalTo(30);
+        make.right.mas_equalTo(-10);
+        make.centerY.equalTo(time);
+        
+    }];
+    
+    //查询按钮
+    UIButton *searchBtn = [[UIButton alloc] init];
+    [searchBtn setImage:[UIImage imageNamed:@"search_btn.png"] forState:UIControlStateNormal];
+    [self.view addSubview:searchBtn];
+    [searchBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.width.mas_equalTo(100);
+        make.height.mas_equalTo(100);
+        make.top.equalTo(time.mas_bottom).offset(130);
+        make.centerX.equalTo(self.view);
+    }];
+    [searchBtn addTarget:self action:@selector(searchBtnClick) forControlEvents:UIControlEventTouchDown];
+    
+
 }
+- (void)searchBtnClick{
+    NSLog(@"www");
+}
+////日期选择器
+//- (void)setDatePick{
+//    //日期时间选择器
+//    UIDatePicker *datePicker = [ [ UIDatePicker alloc] initWithFrame:CGRectMake(0,500,SCREEN_WIDTH,SCREEN_HEIGHT*0.3)];
+//    datePicker.backgroundColor = [UIColor yellowColor];
+//    datePicker.datePickerMode = UIDatePickerModeDate;
+//    datePicker.locale = [NSLocale localeWithLocaleIdentifier:@"zh"];
+//    [self.view addSubview: datePicker];
+//    [datePicker addTarget:self action:@selector(dateChanged:) forControlEvents:UIControlEventValueChanged];
+////    datePicker.hidden = YES;
+//
+//}
+
+//- (void)dateChanged:(UIDatePicker *)datePicker
+//{
+//    NSDate *theDate = datePicker.date;
+//    NSLog(@"%@",[theDate descriptionWithLocale:[NSLocale currentLocale]]);
+//
+//    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+//    dateFormatter.dateFormat = @"YYYY-MM-dd";
+//    NSLog(@"%@",[dateFormatter stringFromDate:theDate]);
+//}
 //点击调用
 - (void)maleBtnClick:(UIButton *)button {
     _maleSelectedButton.selected = NO;
